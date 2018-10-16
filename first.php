54f7ca1525b19931712172377b29947163020996
isami odagiri <gurentosouen@gmail.com>
2018-10-16 14:43:54 +0900

    追加　オダギリ  phpファイルに記載

---
+++ first.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>今回の開発</title>
+</head>
+<body>
+  <h1>開発</h1>
+  <div>
+    メンバー
+    <ul>
+      <li>オダギリ</li>
+      <li>カジハラ</li>
+      <li>シミズ</li>
+      <li>トミナガ</li>
+    </ul>
+  </div>
+</body>
+</html>
